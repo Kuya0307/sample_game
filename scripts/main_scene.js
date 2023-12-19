3f77a433dda6a4f00a43d563a4a528ccd0cdf401
kuya <k.oda.sys22@morijyobi.ac.jp>
2023-12-19 14:17:17 +0900

    taroとhanakoを表示させた #4

--- scripts/main_scene.js
+++ scripts/main_scene.js
@@ -10,11 +10,17 @@ class MainScene extends Phaser.Scene {
     preload() {
         // 画像の読み込み(使用する時の名前, パス)
         this.load.image('back', 'assets/back.jpg');
+        this.load.image('taro','assets/taro.png');
+        this.load.image('hanako','assets/hanako.png');
     }
 
 
     create() {
         this.add.image(400, 300, 'back');
+        const taro = this.physics.add.sprite(50, 50, 'taro');
+        this.taro = taro;
+        const hanako = this.physics.add.sprite(750, 400, 'hanako');
+        this.hanako = hanako;
     }
 
 
